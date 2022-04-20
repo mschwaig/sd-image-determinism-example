15ec23b2c1152ecb9c3b41e15a6d8523f803668a
Martin Schwaighofer <mschwaig@users.noreply.github.com>
2022-04-20 13:17:10 +0200

    add raspberry pi bootloader
    This change is untested since I made it some time ago and don't remember if it worked.

--- configuration.nix
+++ configuration.nix
@@ -3,6 +3,12 @@
     (modulesPath + "/installer/sd-card/sd-image-aarch64.nix")
   ];
 
+  boot.loader.raspberryPi = {
+    enable = true;
+    uboot.enable = true;
+    version = 4;
+  };
+
   users.extraUsers.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILnU1xQN50B54S98io0kH1xElc9yNqmZMPF0s8QASLaB mschwaig@hydralisk"
   ];
